99235294f00f52de0b6594390f4de69c92ec6452
fiqryq <fiqrychoerudin48@gmail.com>
2022-05-03 08:19:11 +0700

    update layout container

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,15 @@
 import { ReactNode } from "react";
 type ILayout = {
+  title?: string;
   children: ReactNode;
 };
 export const Layout = (props: ILayout) => {
-  return <div className="mx-auto max-w-5xl">{props.children}</div>;
+  return (
+    <div className="mx-auto max-w-5xl">
+      <h1 className="my-5 border-b-[1px] pb-5 text-center text-4xl font-bold">
+        {props.title}
+      </h1>
+      {props.children}
+    </div>
+  );
 };
